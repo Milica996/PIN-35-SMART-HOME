0ae7e1ac77b6f06cc0b228f0adea5484a5bc9a22
Milica996 <61465292+Milica996@users.noreply.github.com>
2020-10-28 11:17:07 +0300

    Update main.cpp
    Added commit to main

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
+//programm for print "Hello World" 
+
 int main() {
     char name[20];
     cout << "Hello world! Have a nice day!\n";
     cout << "Enter your name: ";
     cin >> name;
     cout << "Hello, " << name << "!\n";
-    cout << "Nice to see you learning Git\n";
+    cout << "Nice to see you learning Git\n"; 
     system("pause");
     return 0;
 }
